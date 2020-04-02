0c41c6202184405b668214b31be76baa02fe4f86
venkatkasaraneni <vkasaraneni@evtcorp.com>
2020-04-02 13:12:41 -0500

    Update:added a new example for if_cond

---
+++ examples/if_cond.py
@@ -0,0 +1,25 @@
+# Example with numbers
+x = "sumanth"
+if (x=="sumanth"): 
+ print ("x value is sumanth")
+else : 
+ print ("x value is not sumanth")
+
+
+x = 10
+if (x==10):
+ print ("x value is 10")
+
+# Another example 
+
+elephant_weight = 3000
+ant_weight = 20
+print (elephant_weight)
+print (ant_weight)
+
+if elephant_weight > ant_weight:
+ print(ant_weight)
+else:
+ print(elephant_weight)
+
+
